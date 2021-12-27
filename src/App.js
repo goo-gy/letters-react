e394cf20d1a39da129d58ac501010bd12fde2082
goo-gy <lkukh17@naver.com>
2021-12-28 01:39:14 +0900

    directory structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./page/Home";
+import Dropdown from "./shared/Dropdown"
 
 function App() {
   return (
-    <div className="border-4 border-dashed text-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Home />
+      <div className="border-4 border-dashed text-center">
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+        <Dropdown />
+      </div>
     </div>
   );
 }
---
+++ src/page/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='container bg-black'>HAHA</div>
+        </div>
+    );
+};
+
+export default Home;

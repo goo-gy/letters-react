99be1c4e50a76f7845c21cd1176f16d7d64b3305
goo-gy <lkukh17@naver.com>
2021-12-28 23:18:44 +0900

    #4 React Router\n - Home, Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import Home from "./page/Home";
+import { Route, Routes } from "react-router-dom";
+import { Home, Login } from "./page";
 
 function App() {
   return (
-    <div>
-      <Home />
-      <div className="border-4 border-dashed text-center">
-        <h1 className="text-3xl font-bold">Hello world!</h1>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Root.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+class Root extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+              <App/>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default Root;

f87d191928876cc41ed3157143338781117818b7
goo-gy <lkukh17@naver.com>
2022-01-26 12:57:07 +0900

    Clean Project
    - 필요없는 파일 삭제
    - Logo 경로 이동

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-reportWebVitals();
--- src/page/Login/SignIn.jsx
+++ src/page/Login/SignIn.jsx
@@ -1,4 +1,4 @@
-import Logo from 'logo.svg';
+import Logo from 'asset/img/logo.svg';
 
 const SignIn = () => {
   return (

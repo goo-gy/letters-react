8fd8a4d2150c05bf2618ea61f402f77ba4b1eaa6
goo-gy <lkukh17@naver.com>
2022-01-26 21:21:18 +0900

    clean files
    - 필요없는 파일 삭제
    - css 이름 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+// local
+import App from './App';
+import './tailwind.css';
 
 ReactDOM.render(
   <BrowserRouter>

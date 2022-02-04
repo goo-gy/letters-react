4d051c40067ff70a827b0e91270113c2156f4a25
goo-gy <lkukh17@naver.com>
2022-02-04 14:52:03 +0900

    redux persist
    - redux persist를 이용해 redux state를 local storage에 저장하는 효과
    - 일단은 persistReducer와 persistStore만 사용해도 local storage에 잘 저장되는 것을 확인
    - 일반적으로 PersistGate와 함께 사용하던데 알아보고 적용할 예정
    
    # 7

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 // local
 import reducerDarkMode from './reducerDarkMode';
 
-// const rootReducer = combineReducers({ reducerDarkMode });
+const persistConfig = { key: 'root', storage };
 
-export default reducerDarkMode;
+// const rootReducer = combineReducers({ reducerDarkMode });
+const persistedReducer = persistReducer(persistConfig, reducerDarkMode);
+
+export default persistedReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore } from 'redux';
+import { persistStore } from 'redux-persist';
 
 // local
 import rootReducer from './rootReducer.js';
 
 const store = createStore(rootReducer);
+persistStore(store);
 
 export default store;
